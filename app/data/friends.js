e1a4c3c118b3a117c4a6a03ffef82337d2a784e0
Xandromus <xanderrapstine@gmail.com>
2018-04-26 13:12:38 -0500

    consolidated friends.js functionality into apiRoutes. now accesses friends.json directly

--- app/data/friends.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-const path = require("path");
-let raw = fs.readFileSync(path.join(__dirname, "friends.json"));
-let friends = JSON.parse(raw);
-
-module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,15 @@
 module.exports = function apiRoutes(app) {
   const fs = require("fs");
   const path = require("path");
-  let friends = require("./../data/friends.js");
+  let friends = require("./../data/friends.json");
 
   app.get("/api/friends", (req, res) => {
-    return res.json(friends);
+    // friends(friends => res.json(friends));
+    return fs.readFile(path.join(__dirname, '../data/friends.json'), 'utf8', (err, data) => {
+      if (err) throw err;
+      let friendList = JSON.parse(data);
+      res.json(friendList);
+    });
   });
 
   app.post("/api/friends", (req, res) => {
@@ -12,15 +17,6 @@ module.exports = function apiRoutes(app) {
     let differenceArray = [];
     let newfriend = req.body;
     console.log(newfriend);
-
-  //  friends = friends.reduce((carry, friend) => {
-  //   let val = newfriend.scores.reduce(
-  //               (carry, currentScore, i) => carry + Math.abs(friend.scores[i] - currentScore), 
-  //               0
-  //             );
-    
-  //   return val < carry.val ? { target: friend, val: val} : carry;
-  //  }, 100)
 
     for (let i = 0; i < friends.length; i++) {
       totalDifference = 0;
@@ -34,16 +30,16 @@ module.exports = function apiRoutes(app) {
     let match = differenceArray.indexOf(Math.min(...differenceArray));
     console.log(match);
 
-    friends.push(newfriend);
+    // friends.push(newfriend);
 
     fs.readFile(path.join(__dirname, '../data/friends.json'), 'utf8', (err, data) => {
-        if (err) throw err;
-        let json = JSON.parse(data);
-        json.push(newfriend);
+      if (err) throw err;
+      let json = JSON.parse(data);
+      json.push(newfriend);
 
-        fs.writeFile(path.join(__dirname, '../data/friends.json'), JSON.stringify(json, null, 2), (err) => {
-          if (err) throw err;
-        });
+      fs.writeFile(path.join(__dirname, '../data/friends.json'), JSON.stringify(json, null, 2), (err) => {
+        if (err) throw err;
+      });
     });
 
     res.json(friends[match]);
